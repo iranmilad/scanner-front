ffa1b1354445c09a86ef960d5ee46b61982a7dad
farhad <coding.farhad@gmail.com>
2022-08-27 11:48:37 +0430

    fetch removed

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -11,18 +11,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Cache and return requests
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
 
 // Update a service worker
 self.addEventListener('activate', (event) => {
