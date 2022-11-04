7f8514f90ddd436b03d004375783506cc4dceafa
cfarhad <coding.farhad@gmail.com>
2022-11-04 13:52:17 +0330

    footer text changed

--- src/layout/public/components/footer/index.js
+++ src/layout/public/components/footer/index.js
@@ -1,18 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <>
       <footer className="w-full p-4 footer bg-slate-700 ">
-        <div className="container flex items-center justify-between">
+        <div className="container flex items-center justify-center">
           <div>
             <p className="text-white text-sm">
               <span className="text-white">
-              کلیه حقوق این وبسایت متعلق به شرکت ایده نگار ماهان است
+              کلیه حقوق این وبسایت متعلق به <Link><span className="text-sky-500">Tseshow</span></Link> است
               </span>
             </p>
-          </div>
-          <div className="flex items-center">
-            <i className="fa-brands fa-instagram text-slate-300 text-lg"></i>
-            <i className="fa-brands fa-telegram text-slate-300 text-lg mr-10"></i>
           </div>
         </div>
       </footer>
